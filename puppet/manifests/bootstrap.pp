357fc37aeee26878f79b596ae31acd591961b835
Tom Causs <tom.causs@gmail.com>
2014-12-04 16:23:29 +0100

    Add OpenJDK Java runtime

--- puppet/manifests/bootstrap.pp
+++ puppet/manifests/bootstrap.pp
@@ -4,3 +4,9 @@ Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
 class { 'apt':
   apt_update_frequency => weekly
 }
+
+################### Java ####################
+package { 'openjdk-7-jre-headless':
+	require  => Class['apt'],
+	ensure => latest
+}
